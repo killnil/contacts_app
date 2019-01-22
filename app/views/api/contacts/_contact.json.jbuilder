e03f1e75fb18396d9fa9231eeffc842d7385f4e9
JackMarx <seriousfools@gmail.com>
2019-01-22 10:46:51 -0600

    added middle name to contacts

--- app/views/api/contacts/_contact.json.jbuilder
+++ app/views/api/contacts/_contact.json.jbuilder
@@ -1,7 +1,10 @@
 json.id contact.id
+
 json.first_name contact.first_name
+json.middle_name contact.middle_name
 json.last_name contact.last_name
 json.full_name contact.full_name
+
 json.email contact.email
 json.phone_number contact.phone_number
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_175557) do
+ActiveRecord::Schema.define(version: 2019_01_22_164331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_01_15_175557) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "middle_name"
   end
 
 end
