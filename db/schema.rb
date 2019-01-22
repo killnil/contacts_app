4aff0d27648e4537f829ef4c3c3b015a6a24a11f
JackMarx <seriousfools@gmail.com>
2019-01-22 10:54:34 -0600

    added bio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_164331) do
+ActiveRecord::Schema.define(version: 2019_01_22_165314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_01_22_164331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "middle_name"
+    t.text "bio"
   end
 
 end
