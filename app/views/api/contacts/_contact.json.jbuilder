adf858a951b244d9a79f31d2f819b42e9ac18aea
JackMarx <seriousfools@gmail.com>
2019-02-01 12:42:13 -0600

    groups

--- app/views/api/contacts/_contact.json.jbuilder
+++ app/views/api/contacts/_contact.json.jbuilder
@@ -9,6 +9,10 @@ json.email contact.email
 json.phone_number contact.phone_number
 json.bio contact.bio
 
+json.groups do 
+  json.array! contact.groups, partial: 'api/groups/group', as: :group
+end
+
 json.formatted do
   json.created_at contact.friendly_created_at
   json.phone_number contact.japan_phone_number
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,42 @@
 
 
-1000.times do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
+# 1000.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
 
-  contact = Contact.new(
-                        first_name: first_name,
-                        last_name: last_name,
-                        email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
-                        phone_number: Faker::PhoneNumber.phone_number
-                        )
-  contact.save
+#   contact = Contact.new(
+#                         first_name: first_name,
+#                         last_name: last_name,
+#                         email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
+#                         phone_number: Faker::PhoneNumber.phone_number
+#                         )
+#   contact.save
+# end
+
+
+
+ContactGroup.destroy_all
+
+contacts = Contact.all
+group_ids = Group.pluck(:id)
+
+contacts.each do |contact|
+  selected_group_ids = group_ids.sample(rand(2..4))
+
+  selected_group_ids.each do |group_id|
+    ContactGroup.create(contact_id: contact.id, group_id: group_id )
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
